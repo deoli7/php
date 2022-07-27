e2997e259717d467e601e6ecef82160337a0ebb4
deoli7 <tso.ufrrj@gmail.com>
2022-07-26 22:09:56 -0300

    resolvi os conflitos

--- php/index.php
+++ php/index.php
@@ -1,13 +1,7 @@
 <? php
 
-<<<<<<< HEAD
 	echo "Olá Mundo 456";
 
-	echo "Olá Udemy!";
-=======
-	echo "Olá Mundo 123";
-
-	echo "Olá hcode";
->>>>>>> a616bb0d64d747976d331ef3df3c20c319c7bacf
+	echo "Olá Udemy e hcode!";
 
 ?>
